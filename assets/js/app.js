a12d8523fc1d40c402fa7cf9da70156903b8c669
imooclam <dragon158man@gmail.com>
2022-11-16 09:42:35 +0200

    no slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,20 +21,20 @@ scroll()
 $(window).on('load',function(){
 
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:25,
-    nav:true,
-    smartSpeed:700,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:2
-        }
-    }
-})})
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:25,
+//     nav:true,
+//     smartSpeed:700,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:2
+//         }
+//     }
+// })})
